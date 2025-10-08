9beceb79a7135381102cc994934cf500c2e71bf1
jayashree-2005-ias <jayashreevijayakandhan@gmail.com>
2025-10-08 10:20:55 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,16 +1,1 @@
-class students:
-    def_init(name,rollno,marks)
-        self.name=name
-        self.rollno=rollno
-        self.marks=marks
-    def display(self)
-        print(f"name:{self.name}")
-        print(f"rollno:{self.rollno}")
-        print(f"marks:{self.marks}")
-    def search(self)
-        self.name=name
-    def delete(self)
-        self.rollno=rollno
-    def update
-        self.average=average
-        
+
