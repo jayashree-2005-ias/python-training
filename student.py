84a83898ffe7b6c04d914ab33b1b916336bed908
jayashree-2005-ias <jayashreevijayakandhan@gmail.com>
2025-10-08 09:58:14 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,16 @@
+class students:
+    def_init(name,rollno,marks)
+        self.name=name
+        self.rollno=rollno
+        self.marks=marks
+    def display(self)
+        print(f"name:{self.name}")
+        print(f"rollno:{self.rollno}")
+        print(f"marks:{self.marks}")
+    def search(self)
+        self.name=name
+    def delete(self)
+        self.rollno=rollno
+    def update
+        self.average=average
+        
